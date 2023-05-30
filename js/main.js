e110abb34c11679d7244bf39ef111f8835002d56
Asilbestro <asilbestro7@gmail.com>
2023-05-30 09:08:49 -0300

    Se arreglaron algunos bugs que no permitian expandir las tareas que se agregaban.

--- js/main.js
+++ js/main.js
@@ -6,25 +6,59 @@ const taskColumn = document.getElementById("todo-column");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const value = input.value; 
+    const value = input.value;
     if (!value) return;
 
-    const newTask = document.createElement("p");
+    // Crear elemento div que contiene la tarea
+    const div = document.createElement("div");
+    div.className = 'task';
+    div.id = 'taskId';
+    div.draggable = true;
 
-    newTask.classList.add("task");
-    newTask.setAttribute("draggable", "true");
-    newTask.innerText = value;
+    // Crear elemento p dentro el div
+    const p = document.createElement('p');
+    p.textContent = value;
 
-    
+    // Añadir elemento p al div
+    div.appendChild(p);
 
-    newTask.addEventListener("dragstart", () => {
-        newTask.classList.add("is-dragging");
+
+    // Crear elemento div con información adicional dentro del otro div
+    const additionalInfoDiv = document.createElement('div');
+    additionalInfoDiv.className = 'additional-info';
+
+    // Crear elementos p dentro del div con info adicional
+    const descriptionP = document.createElement('p');
+    descriptionP.textContent = 'Descripción de la tarea:';
+    const managerP = document.createElement('p');
+    managerP.textContent = 'Encargado: Juan (CEO)';
+    const estimatedHoursP = document.createElement('p');
+    estimatedHoursP.textContent = 'Horas estimadas: 15hs';
+
+    // Añadir los elementos p al div con info adicional
+    additionalInfoDiv.appendChild(descriptionP);
+    additionalInfoDiv.appendChild(managerP);
+    additionalInfoDiv.appendChild(estimatedHoursP);
+
+    // Añadir div adicional al div principal
+    div.appendChild(additionalInfoDiv);
+
+
+    div.addEventListener("dragstart", () => {
+        div.classList.add("is-dragging");
     });
 
-    newTask.addEventListener("dragend", () => {
-        newTask.classList.remove("is-dragging");
+    div.addEventListener("dragend", () => {
+        div.classList.remove("is-dragging");
     });
 
-    taskColumn.appendChild(newTask);
+    div.addEventListener('click', () => {
+        additionalInfoDiv.classList.toggle("expand");
+    });
+
+    // Añadir el div principal a la columna de tareas
+    taskColumn.appendChild(div);
+
+    // Borrar campo input una vez agregada la tarea
     input.value = "";
 });
