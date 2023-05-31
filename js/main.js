3990294634f72e4d0a79bc1a0fadf47043f629cc
Asilbestro <asilbestro7@gmail.com>
2023-05-31 11:43:53 -0300

    Se arreglaron bugs con la función de explotar cada tarea

--- js/main.js
+++ js/main.js
@@ -1,19 +1,26 @@
-const submit = document.getElementById("btn-id");
-// const input = document.getElementById("todo-input");
-// const task_column = document.getElementById("todo-column");
+const form = document.querySelector(".form");
+const button_add_task = document.getElementById("btn-id");
+
+// Valores del formulario
+const input_task = document.getElementById('name-task');
+const input_description = document.getElementById('description-task');
+const input_expiration = document.getElementById('expiration-task');
+const input_in_charge = document.getElementById('in-charge-task');
+
+const task_column = document.getElementById("todo-column");
+
+const button_close_form = document.getElementById('close-form');
+
 
 // Cuando se hace click al boton añadir tarea, muestra el formulario
-submit.addEventListener("click", (e) => {
+button_add_task.addEventListener("click", (e) => {
     e.preventDefault();
 
-    const form = document.querySelector(".form");
     form.classList.toggle("expand-form");
+});
 
-
-
-    const value = input.value;
-    console.log(value);
-    if (!value) return;
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
 
     // Crear elemento div que contiene la tarea
     const div = document.createElement("div");
@@ -23,7 +30,7 @@ submit.addEventListener("click", (e) => {
 
     // Crear elemento p dentro el div
     const p = document.createElement('p');
-    p.textContent = value;
+    p.textContent = input_task.value;
 
     // Añadir elemento p al div
     div.appendChild(p);
@@ -35,19 +42,26 @@ submit.addEventListener("click", (e) => {
 
     // Crear elementos p dentro del div con info adicional
     const description_p = document.createElement('p');
-    description_p.textContent = 'Descripción de la tarea:';
+    description_p.textContent = 'Descripción de la tarea: ';
+
+    const text_description_p = document.createElement('p');
+    text_description_p.textContent = input_description.value;
+
     const in_charge_p = document.createElement('p');
-    in_charge_p.textContent = 'Encargado: Juan (CEO)';
+    in_charge_p.textContent = 'Encargado: ' + input_in_charge.value;
+
     const estimated_hours_p = document.createElement('p');
-    estimated_hours_p.textContent = 'Horas estimadas: 15hs';
+    estimated_hours_p.textContent = 'Vencimiento: ' + input_expiration.value;
 
     // Añadir los elementos p al div con info adicional
     additional_info_div.appendChild(description_p);
     additional_info_div.appendChild(in_charge_p);
     additional_info_div.appendChild(estimated_hours_p);
+    description_p.appendChild(text_description_p);
 
     // Añadir div adicional al div principal
     div.appendChild(additional_info_div);
+
 
     div.addEventListener("dragstart", () => {
         div.classList.add("is-dragging");
@@ -65,9 +79,22 @@ submit.addEventListener("click", (e) => {
     task_column.appendChild(div);
 
     // Borrar campo input una vez agregada la tarea
-    input.value = "";
+
+    input_task.value = "";
+    input_description.value = "";
+    input_expiration.value = "";
+    input_in_charge.value = "";
+
+    form.classList.toggle("expand-form");
 });
 
-function die(message) {
-    throw new Error(message);
-}
+// Evento para cerrar formulario
+button_close_form.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    form.classList.toggle("expand-form");
+})
+
+// function die(message) {
+//     throw new Error(message);
+// }
