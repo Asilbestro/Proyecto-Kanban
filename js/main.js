10a6fc5127046460dfe6da9cc5b62b19680b30e0
Asilbestro <asilbestro7@gmail.com>
2023-06-12 11:26:28 -0300

    Se unificarón los archivos JS para unirlo al proyecto ya existente

--- js/main.js
+++ js/main.js
@@ -24,6 +24,16 @@ let column_closer = "";
 // event listener cada vez que queremos seleccionar un elemento 
 board1.addEventListener('click', (event) => {
 
+    // expandir tarea al clickear cada una de ellas
+    if (event.target && event.target.id === "taskId" || event.target.tagName === "P" || event.target.id === ".additional-info") {
+        // columna de donde se activo el evento añadir tarea
+        const parent = event.target;
+        const column_close = parent.closest('.task');
+
+        const additional_info = column_close.querySelector('.additional-info');
+        additional_info.classList.toggle("expand");
+    }
+
     // si se hace click al boton para añadir tarea, mustra el formulario
     if (event.target && event.target.id === 'button-id') {
         event.preventDefault();
@@ -350,3 +360,65 @@ function die(message) {
     throw new Error(message);
 }
 
+
+// Drag y drop
+board1.addEventListener("dragstart", (event) => {
+    if (event.target && event.target.id === "taskId" && event.target.draggable) {
+        const task = event.target;
+
+        // cerrar el div que muestra mas opciones a las tareas (editar,eliminar, cambiar de color)
+        task.querySelector(".container-functions").classList.remove("hidden");
+
+        task.classList.add("is-dragging");
+    }
+})
+
+board1.addEventListener("dragend", (event) => {
+    if (event.target && event.target.id === "taskId" && event.target.draggable) {
+        const task = event.target;
+
+        task.classList.remove("is-dragging");
+    }
+})
+
+board1.addEventListener("dragover", (event) => {
+    event.preventDefault();
+
+    if (event.target && event.target.id === "todo-column") {
+        const zone = event.target;
+
+
+        const bottom_task = insertAboveTask(zone, event.clientY);
+
+        // busca el elemento que tenga la clase .is-dragging, es decir, que está siendo arrastrado
+        const current_task = document.querySelector(".is-dragging");
+
+        if (!bottom_task) {
+            zone.appendChild(current_task);
+        } else {
+            zone.insertBefore(current_task, bottom_task);
+        }
+    }
+})
+
+// Lógica para insertar tarea arriba o abajo de la tarea existente
+function insertAboveTask(zone, mouseY) {
+    const non_dragging_tasks = zone.querySelectorAll(".task:not(.is-dragging)");
+
+    let closest_task = null;
+    let closest_offset = Number.NEGATIVE_INFINITY;
+
+    non_dragging_tasks.forEach((task) => {
+
+        // indica la posición vertical del elemento task 
+        const { top } = task.getBoundingClientRect();
+        const offset = mouseY - top;
+
+        if (offset < 0 && offset > closest_offset) {
+            closest_offset = offset;
+            closest_task = task;
+        }
+    });
+
+    return closest_task;
+}
